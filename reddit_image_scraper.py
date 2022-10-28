3586885b84f68a245bc9a87c458a5310bb315791
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-10-28 21:37:41 +0100

    works mostly ok now on rpics

--- reddit_image_scraper.py
+++ reddit_image_scraper.py
@@ -1,16 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests, time, shutil, os
-from webdriver_manager.firefox import GeckoDriverManager
+import requests, time, os
 
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-# options = webdriver.FirefoxOptions()
-# options.add_argument('-headless')
-
-
+driver = webdriver.Firefox(executable_path='/Users/richardgannon/geckodriver')
 driver.get("https://www.reddit.com/r/pics/")
-# time.sleep(5)
+
 print('finding button')
 try:
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div[1]/section/div/section[2]/section[1]/form/button').click()
@@ -21,12 +16,13 @@ except:
 input("Press enter to continue")
 pages = int(input('How many pages? '))
 pages_list = []
-image_urls = []
-for i in range(pages):
-    print('scrolling...')
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    print('waiting for loads...')
-    time.sleep(5)
+# image_urls = []
+if pages > 0:
+    for i in range(pages):
+        print('scrolling...')
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        print('waiting for loads...')
+        time.sleep(5)
 posts = driver.find_elements(By.XPATH, '//img[@alt="Post image"]')
 
 for i in range(len(posts)):
@@ -45,16 +41,24 @@ print('found ' + str(len(pages_list)) + ' image posts')
 for page in pages_list:
     print(page)
     save_name = str(page).split('/')[7]
-    breakpoint()
     driver.get(page) # go to the post
+    div_iterator = 1
+    image_url = None
+    while image_url is None:
+        try:
+            image_url = driver.find_element(By.XPATH, f'/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[1]/div/div[{div_iterator}]/div/a').get_attribute('href') # gets the image URL from the post page
+            print('image url found')
+        except:
+            print('image url not found')
+            pass
+        div_iterator += 1
 
-    image_url = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[1]/div/div[5]/div/a').get_attribute('href') # gets the image URL from the post page
 
-    res = requests.get(image_url)
     try:
+        res = requests.get(image_url)
         res.raise_for_status()
         print('image found')
-        image_urls.append(image_url)
+        # image_urls.append(image_url)
         print('image url added to list')
         image_file = open(os.path.join('reddit_images', os.path.basename(save_name + '.jpg')), 'wb')
         print(f'image file opened as {save_name}')
@@ -70,9 +74,9 @@ for page in pages_list:
     print('moving to next post \n \n')
 
 
-print('found ' + str(len(image_urls)) + ' image urls')
-for url in image_urls:
-    print(url)
+# print('found ' + str(len(image_urls)) + ' image urls')
+# for url in image_urls:
+#     print(url)
 
 
 # /html/body/div[1]/div/div[2]/div[3]/div/div/div/div[2]/div[1]/div[2]/div[1]/div/div[3]
