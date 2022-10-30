bfe03746a569af477fcbd338c0d5f6a9b5987b8d
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-10-30 18:20:30 +0000

    2nd attempt at image scraper

--- reddit_image_scraper.py
+++ reddit_image_scraper.py
@@ -3,75 +3,107 @@ from selenium.webdriver.common.by import By
 import requests, time, os
 
 
-driver = webdriver.Firefox(executable_path='/Users/richardgannon/geckodriver')
-driver.get("https://www.reddit.com/r/pics/")
 
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options, executable_path='/Users/richardgannon/geckodriver')
+# pages = int(input('How many pages? '))
+pages = 10
+# subreddit = input('Subreddit? ')
+subreddit = 'pics'
+driver.get(f'https://www.reddit.com/r/{subreddit}/top/?t=all')
+
+
+### cookies accepter
 print('finding button')
 try:
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div[1]/section/div/section[2]/section[1]/form/button').click()
     print('found and clicked button')
 except:
     print('no button')
-# driver.switch_to.alert.accept()
-input("Press enter to continue")
-pages = int(input('How many pages? '))
-pages_list = []
-# image_urls = []
+### cookies accepter
+
+# input('Press enter to continue')
+
+
 if pages > 0:
     for i in range(pages):
         print('scrolling...')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         print('waiting for loads...')
         time.sleep(5)
-posts = driver.find_elements(By.XPATH, '//img[@alt="Post image"]')
 
-for i in range(len(posts)):
+try:
+    print('getting post url...')
+    all_links = driver.find_elements(By.TAG_NAME, 'a')
+    print('found some posts')
+except:
+    print('...no post url found')
+    pass
+
+
+
+print('found ' + str(len(all_links)) + ' links')
+link_list = []
+for post in all_links:
     try:
-        print('getting post url...')
-        xpath_string = f'/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[4]/div[1]/div[4]/div[{i+3}]/div/div/div[3]/div[3]/div/div[2]/div/a'
-        post_url = driver.find_element(By.XPATH, xpath_string).get_attribute('href')
-        print('adding to list...')
-        pages_list.append(post_url)
+        post = post.get_attribute('href')
+        if post.startswith('https://www.reddit.com/r/pics/comments/'):
+            link_list.append(post)
+        print(f'added {post} to list')
     except:
-        print('no post url found')
+        print('could not add to list')
         pass
 
-
-print('found ' + str(len(pages_list)) + ' image posts')
-for page in pages_list:
-    print(page)
-    save_name = str(page).split('/')[7]
-    driver.get(page) # go to the post
-    div_iterator = 1
-    image_url = None
-    while image_url is None:
-        try:
-            image_url = driver.find_element(By.XPATH, f'/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[1]/div/div[{div_iterator}]/div/a').get_attribute('href') # gets the image URL from the post page
-            print('image url found')
-        except:
-            print('image url not found')
-            pass
-        div_iterator += 1
+link_set = set(link_list)
+print('found ' + str(len(link_set)) + ' posts')
+for post in link_set:
+    print(post)
 
 
+# breakpoint()
+for page in link_set:
     try:
-        res = requests.get(image_url)
-        res.raise_for_status()
-        print('image found')
-        # image_urls.append(image_url)
-        print('image url added to list')
-        image_file = open(os.path.join('reddit_images', os.path.basename(save_name + '.jpg')), 'wb')
-        print(f'image file opened as {save_name}')
-        # image_file = open(f'reddit_images/{save_name}', 'wb')
-        for chunk in res.iter_content(100000):
-            image_file.write(chunk)
-        print(f'image saved as {save_name}')
-        image_file.close()
-        print(f'image file closed')
-    except Exception as exc:
-        print(exc)
+        print(page)
+        save_name = str(page).split('/')[7]
+        driver.get(page) # go to the post
+        links_list = driver.find_elements(By.TAG_NAME, 'a') # get all the links
+        for link in links_list:
+            link = link.get_attribute('href')
+            if link.endswith('.jpg') or link.endswith('.png') or link.endswith('.gif'):
+                print('saving image...')
+                r = requests.get(link)
+                with open(f'reddit_images/{save_name}.jpg', 'wb') as f:
+                    f.write(r.content)
+                print('saved image')
+                break
+            else:
+                print('...no image found')
+                pass
+    except Exception as e:
+        print(e)
         pass
-    print('moving to next post \n \n')
+
+
+
+#     try:
+#         res = requests.get(image_url)
+#         res.raise_for_status()
+#         print('image found')
+#         # image_urls.append(image_url)
+#         print('image url added to list')
+#         image_file = open(os.path.join('reddit_images', os.path.basename(save_name + '.jpg')), 'wb')
+#         print(f'image file opened as {save_name}')
+#         # image_file = open(f'reddit_images/{save_name}', 'wb')
+#         for chunk in res.iter_content(100000):
+#             image_file.write(chunk)
+#         print(f'image saved as {save_name}')
+#         image_file.close()
+#         print(f'image file closed')
+#     except Exception as exc:
+#         print(exc)
+#         pass
+#     print('moving to next post \n \n')
 
 
 # print('found ' + str(len(image_urls)) + ' image urls')
