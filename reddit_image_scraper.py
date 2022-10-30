544853a2cb5d370adbbbc21d3f6ec3ecb6f91c58
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-10-30 21:36:18 +0000

    added options to start of script

--- reddit_image_scraper.py
+++ reddit_image_scraper.py
@@ -5,13 +5,74 @@ import requests, time, os
 
 
 options = webdriver.FirefoxOptions()
+headless = None
+if os.path.exists('reddit_images'):
+    pass
+else:
+    os.mkdir('reddit_images')
+while True:
+    headless = input("Do you want to run the scraper headless? (y/n): ")
+    if headless == "y":
+        options.add_argument("--headless")
+        break
+    elif headless == "n":
+        break
+    else:
+        print("Please enter a valid option.")
+
+
 options.add_argument('--headless')
+pages = int(input('How many pages? '))
+subreddit = input('Subreddit? ')
+if os.path.exists('reddit_images/' + subreddit):
+    pass
+else:
+    os.mkdir('reddit_images/' + subreddit)
+while True:
+    top = input("Top, Hot, New, or Rising? (t/h/n/r): ")
+    after = ""
+    if top == "t":
+        top = "top"
+        after = input("After what time? (day/week/month/year/all): ")  
+        if after == "d":
+            after = ""
+            break
+        elif after == "w":
+            after = "&t=week"
+            break
+        elif after == "m":
+            after = "&t=month"
+            break
+        elif after == "y":
+            after = "&t=year"
+            break
+        elif after == "a":
+            after = "&t=all"
+            break
+        else:
+            print("Please enter a valid option.")        
+        break
+    elif top == "h":
+        top = "hot"
+        break
+    elif top == "n":
+        top = "new"
+        break
+    elif top == "r":
+        top = "rising"
+        break
+    elif top == "c":
+        top = "controversial"
+        break
+    else:
+        print("Please enter a valid option.")
+
+url = 'https://www.reddit.com/r/' + subreddit + '/' + top.lower() + '/?count=25&after=t3_' + after.lower()
+print(f'Retrieving {pages} pages from {url}')
+print('Please wait...')
 driver = webdriver.Firefox(options=options, executable_path='/Users/richardgannon/geckodriver')
-# pages = int(input('How many pages? '))
-pages = 10
-# subreddit = input('Subreddit? ')
-subreddit = 'pics'
-driver.get(f'https://www.reddit.com/r/{subreddit}/top/?t=all')
+
+driver.get(url)
 
 
 ### cookies accepter
@@ -23,7 +84,8 @@ except:
     print('no button')
 ### cookies accepter
 
-# input('Press enter to continue')
+if headless == "n":
+    input('Press enter to continue')
 
 
 if pages > 0:
@@ -33,85 +95,70 @@ if pages > 0:
         print('waiting for loads...')
         time.sleep(5)
 
-try:
-    print('getting post url...')
-    all_links = driver.find_elements(By.TAG_NAME, 'a')
-    print('found some posts')
-except:
-    print('...no post url found')
-    pass
-
-
-
+all_links = driver.find_elements(By.TAG_NAME, 'a')
 print('found ' + str(len(all_links)) + ' links')
 link_list = []
 for post in all_links:
     try:
         post = post.get_attribute('href')
-        if post.startswith('https://www.reddit.com/r/pics/comments/'):
+        if post.startswith(f'https://www.reddit.com/r/{subreddit}/comments/'):
             link_list.append(post)
-        print(f'added {post} to list')
     except:
-        print('could not add to list')
         pass
 
 link_set = set(link_list)
 print('found ' + str(len(link_set)) + ' posts')
-for post in link_set:
-    print(post)
 
-
-# breakpoint()
+print('getting images...')
+image_number = 1
 for page in link_set:
     try:
+        print(f'getting page {str(image_number)} of {str(len(link_set))}')
         print(page)
         save_name = str(page).split('/')[7]
-        driver.get(page) # go to the post
-        links_list = driver.find_elements(By.TAG_NAME, 'a') # get all the links
+        driver.get(page)
+        links_list = driver.find_elements(By.TAG_NAME, 'a')
         for link in links_list:
             link = link.get_attribute('href')
-            if link.endswith('.jpg') or link.endswith('.png') or link.endswith('.gif'):
+            if link.endswith('.jpg') or link.endswith('.png') or link.endswith('.gif') or link.endswith('.jpeg') or link.endswith('.gifv') or link.endswith('.mp4') or link.endswith('.webm'):
                 print('saving image...')
                 r = requests.get(link)
-                with open(f'reddit_images/{save_name}.jpg', 'wb') as f:
-                    f.write(r.content)
-                print('saved image')
+                if link.endswith('.jpg'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.jpg', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.png'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.png', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.gif'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.gif', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.jpeg'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.jpeg', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.gifv'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.gif', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.mp4'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.mp4', 'wb') as f:
+                        f.write(r.content)
+                elif link.endswith('.webm'):
+                    with open(f'reddit_images/{subreddit}/{save_name}.webm', 'wb') as f:
+                        f.write(r.content)
+                else:
+                    print('could not save image')
+                    pass
+                print(f'saved image as {save_name}')
                 break
             else:
-                print('...no image found')
                 pass
     except Exception as e:
         print(e)
         pass
+    image_number += 1
 
 
 
-#     try:
-#         res = requests.get(image_url)
-#         res.raise_for_status()
-#         print('image found')
-#         # image_urls.append(image_url)
-#         print('image url added to list')
-#         image_file = open(os.path.join('reddit_images', os.path.basename(save_name + '.jpg')), 'wb')
-#         print(f'image file opened as {save_name}')
-#         # image_file = open(f'reddit_images/{save_name}', 'wb')
-#         for chunk in res.iter_content(100000):
-#             image_file.write(chunk)
-#         print(f'image saved as {save_name}')
-#         image_file.close()
-#         print(f'image file closed')
-#     except Exception as exc:
-#         print(exc)
-#         pass
-#     print('moving to next post \n \n')
 
-
-# print('found ' + str(len(image_urls)) + ' image urls')
-# for url in image_urls:
-#     print(url)
-
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div/div/div[2]/div[1]/div[2]/div[1]/div/div[3]
 print('script complete')
 # driver.quit()
 
